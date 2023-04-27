4f453d48affb2fd6596946adc4c5a23d88eecfab
PegiMasterGames <112945193+PegiMasterGames@users.noreply.github.com>
2023-04-27 02:10:10 +0200

    Update fetchNews.js

--- fetchNews.js
+++ fetchNews.js
@@ -1,5 +1,5 @@
 async function fetchNews(page, pageSize) {
-  const API_KEY = YOUR_API_KEY;
+  const API_KEY = '53cb5e21d56e47928c007bde089e7f8c'; // API-KEY 
   const url = `https://newsapi.org/v2/top-headlines?page=${page}&pageSize=${pageSize}&apiKey=${API_KEY}`;
 
   const response = await fetch(url);
